d756d4bfde7592c3efae07611b48ce1b58d7e88a
Deepak Hagadur Bheemaraju <deepakhb2@gmail.com>
2024-07-06 22:43:38 +0100

    Remove packages and update org-roam config

--- config.el
+++ config.el
@@ -82,20 +82,7 @@
 
 ;; org-roam config
 (use-package! websocket
-  :after org-roam
-  (setq org-roam-directory "~/projects/fc-notes"))
-
-(use-package! org-roam-ui
-  :after org-roam ;; or :after org
-  ;;         normally we'd recommend hooking orui after org-roam, but since org-roam does not have
-  ;;         a hookable mode anymore, you're advised to pick something yourself
-  ;;         if you don't care about startup time, use
-  ;;  :hook (after-init . org-roam-ui-mode)
-  :config
-  (setq org-roam-ui-sync-theme t
-        org-roam-ui-follow t
-        org-roam-ui-update-on-save t
-        org-roam-ui-open-on-start t))
+  :after org-roam)
 
 
 ;; deft config
